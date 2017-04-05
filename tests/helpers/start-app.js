c91f3b192dbd0278f3826d8fed22e388422047a7
Roxanne Panthaky <rpanthak@ciena.com>
2017-04-05 15:57:13 -0400

    upgrade ember to 2.11

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,17 +1,13 @@
-/* eslint-disable ember-standard/destructure */
-
+import Ember from 'ember'
+const {merge, run} = Ember
 import Application from '../../app'
 import config from '../../config/environment'
-import Ember from 'ember'
-const {run} = Ember
-
-const assign = Object.assign || Ember.assign || Ember.merge
 
 export default function startApp (attrs) {
   let application
 
-  let attributes = assign({}, config.APP)
-  attributes = assign(attributes, attrs) // use defaults, but you can override
+  let attributes = merge({}, config.APP)
+  attributes = merge(attributes, attrs) // use defaults, but you can override;
 
   run(() => {
     application = Application.create(attributes)
