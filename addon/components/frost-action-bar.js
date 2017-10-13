8e922aa3fb182b4f2e693e796a9b7b94fe60f49c
Joshua Scharf <jscharf@ciena.com>
2017-10-13 16:39:24 -0400

    Add ability to show controls based on list item type

--- addon/components/frost-action-bar.js
+++ addon/components/frost-action-bar.js
@@ -3,12 +3,13 @@
  */
 
 import Ember from 'ember'
-const {isEmpty} = Ember
+const {get, getWithDefault, isEmpty, isPresent, typeOf} = Ember
 import computed, {readOnly} from 'ember-computed-decorators'
 import {Component} from 'ember-frost-core'
 import {PropTypes} from 'ember-prop-types'
 
 import layout from '../templates/components/frost-action-bar'
+import {applicableControls} from '../utils/controls'
 
 export default Component.extend({
 
@@ -22,7 +23,10 @@ export default Component.extend({
 
   propTypes: {
     // options
-    controls: PropTypes.arrayOf(PropTypes.EmberComponent).isRequired,
+    controls: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.EmberComponent).isRequired,
+      PropTypes.object.isRequired
+    ]),
     // FIXME: for next major release, make this `i18n.messages.selectedItems()` (@job13er 2017-06-06)
     i18n: PropTypes.shape({
       formatSelectedItemsLabel: PropTypes.func.isRequired
@@ -31,6 +35,11 @@ export default Component.extend({
       PropTypes.EmberObject,
       PropTypes.object
     ])).isRequired,
+    itemTypeKey: PropTypes.string,
+    componentKeyNamesForTypes: PropTypes.oneOfType([
+      PropTypes.EmberObject,
+      PropTypes.object
+    ]),
 
     // callbacks
     getSelectedItemsLabel: PropTypes.func
@@ -66,9 +75,43 @@ export default Component.extend({
   @computed('selectedItems.[]')
   selectedItemsLabel (selectedItems) {
     return this.i18n.formatSelectedItemsLabel(selectedItems.length)
-  }
+  },
+
+  @readOnly
+  @computed('controls')
+  isControlsObject (controls) {
+    return typeOf(controls) === 'object'
+  },
+
+  @readOnly
+  @computed('isVisible', 'isControlsObject', 'controls', 'selectedItems.[]')
+  _controls (isVisible, isControlsObject, controls, selectedItems) {
+    if (isVisible && isControlsObject && isPresent(controls) && isPresent(selectedItems)) {
+      return applicableControls(this.selectedTypesWithControls(selectedItems)).map((controlName) => {
+        return controls[controlName]
+      })
+    }
+
+    return controls
+  },
 
   // == Functions =============================================================
+
+  selectedTypesWithControls (selectedItems) {
+    const componentKeyNamesForTypes = this.get('componentKeyNamesForTypes')
+    const itemTypeKey = this.get('itemTypeKey')
+    const componentKeyNames = this.get('componentKeyNames')
+
+    if (isPresent(componentKeyNamesForTypes) && isPresent(itemTypeKey)) {
+      return selectedItems.reduce((typesWithControls, item) => {
+        const itemType = get(item, itemTypeKey)
+        const itemTypeContent = getWithDefault(componentKeyNamesForTypes, itemType, {})
+        const itemTypeContentControls = getWithDefault(itemTypeContent, get(componentKeyNames, 'controls'), [])
+        typesWithControls[itemType] = itemTypeContentControls
+        return typesWithControls
+      }, {})
+    }
+  }
 
   // == DOM Events ============================================================
 
